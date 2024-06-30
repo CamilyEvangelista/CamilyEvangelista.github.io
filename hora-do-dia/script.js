7383802f99511908a5fac40733ffa71c388590c1
CamilyEvangelista <116023500+CamilyEvangelista@users.noreply.github.com>
2024-06-30 17:30:15 -0300

    corrigir data

--- hora-do-dia/script.js
+++ hora-do-dia/script.js
@@ -2,8 +2,8 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 19
+var hora = data.getHours()
+//var hora = 19
 var minuto = data.getMinutes()
 
 if (hora >1 && hora <23){
