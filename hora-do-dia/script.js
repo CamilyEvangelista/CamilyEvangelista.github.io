f63512aa48ab4d7e7eee139e1e015f47c918cd5b
CamilyEvangelista <116023500+CamilyEvangelista@users.noreply.github.com>
2024-06-30 17:22:10 -0300

    adicionando primeiros projetos

---
+++ hora-do-dia/script.js
@@ -0,0 +1,27 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 19
+var minuto = data.getMinutes()
+
+if (hora >1 && hora <23){
+msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+}
+else {
+    msg.innerHTML = `Agora são ${hora} hora e ${minuto} minutos.`
+}
+if (hora >= 0 && hora < 12) {
+    img.src = 'Manha.png'
+    document.body.style.background = '#bb7d5c'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        img.src = 'Tarde.png'
+        document.body.style.background = '#afbec5'
+    } else {
+        img.src = 'Noite.png'
+        document.body.style.background = '#1b2b44'
+    }
+
+}
